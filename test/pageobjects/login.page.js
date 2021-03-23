9a2297d5d780266756db2c45e27124cf964a69b7
Oksana Akhadova <o.akhadova@gmail.com>
2021-03-23 22:33:51 +0200

    Negative test-cases added

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,9 +1,14 @@
 import Page from './page';
 
-class LoginPage extends Page {
-    get inputUsername () { return $('#normal_login_email') }
-    get inputPassword () { return $('#normal_login_password') }
-    get buttonSubmit () { return $('.login-form-button') }
+class LoginPage extends Page { // СЕЛЕКТОРЫ
+    get inputUsername () { return $('#normal_login_email'); }
+    get inputPassword () { return $('#normal_login_password'); }
+    get buttonSubmit () { return $('.login-form-button'); }
+    get errorToast () { return $('.ant-notification-notice-message'); }
+
+    open () { //МЕТОД
+        return super.open('/');
+    }
 
     setLogin (email) {
         this.inputUsername.setValue(email)
@@ -17,9 +22,15 @@ class LoginPage extends Page {
         this.buttonSubmit.click();
     }
 
-    open () {
-        return super.open('/');
+    submitButtonIsDisabled() { //МЕТОД
+        expect(this.buttonSubmit).toBeDisabled();
     }
+
+    errorToastAppeared() {
+        expect(this.errorToast).toBeDisplayed();
+    }
+
+
 }
 
 export default new LoginPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -2,12 +2,32 @@ import LoginPage from  '../pageobjects/login.page';
 import ProfilePage from '../pageobjects/profile.page'
 
 describe('Auth', () => {
-    it('user logs in with valid data', () => {
+    beforeEach(() => {
         LoginPage.open();
+    });
+
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    })
+
+    it('user logs in with valid data', () => {
         LoginPage.setLogin('kremenchugnails@gmail.com');
         LoginPage.setPassword('test123456');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
     });
+
+    it('submit button is disabled if login and password are absent', ()=> {
+        LoginPage.submitButtonIsDisabled();
+    });
+
+    it('fails if invalid data is provided', () => {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+    });
+
 });
+
 
